d81a5db765cdca432dbd5431b3ef0255a1cc3177
igorlo <9440533@gmail.com>
2019-12-08 18:00:19 +0300

    Костыль

--- src/main/java/com/gamedevcompanyname/beastmaster/game/Files/DislocationTypesReader.java
+++ src/main/java/com/gamedevcompanyname/beastmaster/game/Files/DislocationTypesReader.java
@@ -12,37 +12,30 @@ import java.util.List;
 
 public class DislocationTypesReader {
 
-    public static List<Dislocation.DislocationType> readLocations(){
+    private static String lines = "*****DANGER_LVL|TREASSURENESS|NAME|DECRIPTION*****\n" +
+            "0|10|Эльфийский Лес|Светлое, красивое и безопасное место. Прежде здесь " +
+            "жили эльфы. Возможно вы сможете найти здесь что-нибудь полезное\n" +
+            "1|20|Скорпионья Пустошь|Несмотря на отсутствие углов, опасность поджидает " +
+            "вас буквально повсюду. Однако, проходившие мимо караваны могли оставить здесь что-нибудь ценное.\n" +
+            "2|30|Пещера Огра|Это мрачное место буквально пропитано тревогой и страхом. " +
+            "Вам лучше не рисковать и поскорее убраться отсюда," +
+            " пока на вас не напал серьезный противник.";
 
-        File fileOfLocations = null;
-        try {
-            fileOfLocations = new ClassPathResource("gamedata/Locations.txt").getFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static List<Dislocation.DislocationType> readLocations() {
 
         final List<Dislocation.DislocationType> listOfLocations = new ArrayList<>();
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileOfLocations));
-            String line = reader.readLine();
-            line = reader.readLine();
+        String[] splited = lines.split("\n");
 
-            while (line != null){
-                final String[] parameters = line.split("\\|");
-                final int dangerLvl = Integer.parseInt(parameters[0]);
-                final int treassureness = Integer.parseInt(parameters[1]);
-                final String name = parameters[2];
-                final String description = parameters[3];
-                final Dislocation.DislocationType disType = new Dislocation.DislocationType(name, description, dangerLvl, treassureness);
-                listOfLocations.add(disType);
-                line = reader.readLine();
-            }
-
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 1; i < splited.length; i++) {
+            final String line = splited[i];
+            final String[] parameters = line.split("\\|");
+            final int dangerLvl = Integer.parseInt(parameters[0]);
+            final int treassureness = Integer.parseInt(parameters[1]);
+            final String name = parameters[2];
+            final String description = parameters[3];
+            final Dislocation.DislocationType disType = new Dislocation.DislocationType(name, description, dangerLvl, treassureness);
+            listOfLocations.add(disType);
         }
 
         return listOfLocations;
--- src/main/java/com/gamedevcompanyname/beastmaster/game/Files/InteractablesReader.java
+++ src/main/java/com/gamedevcompanyname/beastmaster/game/Files/InteractablesReader.java
@@ -1,47 +1,35 @@
 package com.gamedevcompanyname.beastmaster.game.Files;
 
 import com.gamedevcompanyname.beastmaster.game.Elements.Interactable;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class InteractablesReader {
 
-    public static List<Interactable> readInteractables(){
+    private static String lines = "*****NAME|TEXT_ON_INTERACTION*****\n" +
+            "Камень с рунами|Поистине древний объект. " +
+            "Письменности на его поверхности, кажется, " +
+            "больше лет, чем самым древним тварям, " +
+            "встреченным вами. Кто знает, может быть он " +
+            "стоял здесь с самого Начала.|false\n" +
+            "Спящий голем|Лучше не трогать этого парня. " +
+            "Кажется, он спит здесь не первое столетие. " +
+            "Вряд ли он будет сильно рад пробуждению.|false";
 
-        File fileOfInteractables = null;
-        try {
-            fileOfInteractables = new ClassPathResource("gamedata/Interactibles.txt").getFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static List<Interactable> readInteractables() {
 
         final List<Interactable> listOfInteractables = new ArrayList<>();
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileOfInteractables));
-            String line = reader.readLine();
-            line = reader.readLine();
+        String[] splited = lines.split("\n");
 
-            while (line != null){
-                final String[] nameAndText = line.split("\\|");
-                final String name = nameAndText[0];
-                final String text = nameAndText[1];
-                final boolean isUniq = Boolean.parseBoolean(nameAndText[2]);
-                final Interactable interactable = new Interactable(name, text, isUniq);
-                listOfInteractables.add(interactable);
-                line = reader.readLine();
-            }
-
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 1; i < splited.length; i++) {
+            final String line = splited[i];
+            final String[] nameAndText = line.split("\\|");
+            final String name = nameAndText[0];
+            final String text = nameAndText[1];
+            final boolean isUniq = Boolean.parseBoolean(nameAndText[2]);
+            final Interactable interactable = new Interactable(name, text, isUniq);
+            listOfInteractables.add(interactable);
         }
 
         return listOfInteractables;
